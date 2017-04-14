920a2905bb49f5d12d7965923dc9a9c2cff0e305
Sam Ma <sam@sams-mbp.gateway.pace.com>
2017-04-14 11:18:01 -0700

    lab1

---
+++ netty-pipe2/src/team/share/QosWorker.java
@@ -0,0 +1,12 @@
+package team.share;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by sam on 3/19/17.
+ */
+public class QosWorker {
+
+    Queue q = new LinkedList();
+}
